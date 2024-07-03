bcac0dec46a7121da662cca886cd08fb6c817374
DemiiD2 <demiandev2@gmail.com>
2024-07-03 13:56:15 -0300

    first commit

---
+++ src/routes/historia/+page.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+	<title>Mi historia</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Como el network Marketing cambió mi vida</h1>
+	<p>HOla soy FABI y te voy a contar por qué el network es 
+		mejor que tu laburo de 8 horas negro de mierda</p>
+	<p>
+		en este capitulo vas a entender cómo pasar de laburar 8 a 12 horas por día a vivir
+		de los negocio digitale
+	</p>
+	<p>Gordo laburador negro asqueroso</p>
+	<br>
+	<p>Como hacer marihuana con javascript y maizena</p>
+	<p>Top 10 intros del animé</p>
+</div>
